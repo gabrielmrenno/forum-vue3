12f0175ec0b1b548beb77415e084ea8d6c6c24e2
Gabriel Renno <gabrielmrenno@gmail.com>
2024-01-15 11:46:21 -0300

    chore: firebase config, add: data to firestore via script

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
+import { initializeApp } from 'firebase/app'
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import firebaseConfig from './config/firebase'
 import router from './router'
 import store from './store'
 
 import BaseDateVue from './components/BaseDate.vue'
+
+const app = initializeApp(firebaseConfig)
 
 const forumApp = createApp(App)
 
